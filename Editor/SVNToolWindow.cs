7bf7fc730a92bf82ba30f221a210ba25ad788138
Dead-Rabbit <UPC_wangzixiao@outlook.com>
2020-10-10 17:45:42 +0800

    修修剪剪

--- Editor/SVNToolWindow.cs
+++ Editor/SVNToolWindow.cs
@@ -867,39 +867,4 @@ public sealed class SVNToolWindow : EditorWindow
         if (!ifExist)
             hideResult.Add(file);
     }
-    
-    public static string HandleCopyPaste(int controlID)
-    {
-        if (controlID == GUIUtility.keyboardControl)
-        {
-            if (Event.current.type == UnityEngine.EventType.KeyUp && (Event.current.modifiers == EventModifiers.Control || Event.current.modifiers == EventModifiers.Command))
-            {
-                if (Event.current.keyCode == KeyCode.C)
-                {
-                    Event.current.Use();
-                    TextEditor editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
-                    editor.Copy();
-                }
-                else if (Event.current.keyCode == KeyCode.V)
-                {
-                    Event.current.Use();
-                    TextEditor editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
-                    editor.Paste();
-#if UNITY_5_3_OR_NEWER || UNITY_5_3
-                    return editor.text; //以及更高的unity版本中editor.content.text已经被废弃，需使用editor.text代替
-#else
-                    return editor.content.text;
-#endif
-                }
-                else if (Event.current.keyCode == KeyCode.A)
-                {
-                    Event.current.Use();
-                    TextEditor editor = (TextEditor)GUIUtility.GetStateObject(typeof(TextEditor), GUIUtility.keyboardControl);
-                    editor.SelectAll();
-                }
-            }
-        }
-        return null;
-    }
-    
 }
